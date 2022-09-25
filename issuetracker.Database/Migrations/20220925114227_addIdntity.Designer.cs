374b15e5b3b56a1f6af72d7833fff7df6f933400
mosaab-rady <mosp.rady@gmail.com>
2022-09-25 23:46:55 +0200

    add Identity

--- issuetracker.Database/Migrations/20220925114227_addIdntity.Designer.cs
+++ issuetracker.Database/Migrations/20220925114227_addIdntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using issuetracker.Database;
@@ -11,9 +12,10 @@ using issuetracker.Database;
 namespace issuetracker.Database.Migrations
 {
     [DbContext(typeof(PostgresContext))]
-    partial class PostgresContextModelSnapshot : ModelSnapshot
+    [Migration("20220925114227_addIdntity")]
+    partial class addIdntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
