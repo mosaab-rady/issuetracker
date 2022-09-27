07437918250fd5cd2ed2197a909b941cf6c3d2a0
mosaab-rady <mosp.rady@gmail.com>
2022-09-27 23:07:22 +0200

    update the project action to get the projectDetailViewModel

--- issuetracker.mvc/Controllers/ProjectsController.cs
+++ issuetracker.mvc/Controllers/ProjectsController.cs
@@ -2,6 +2,7 @@ using issuetracker.Entities;
 using issuetracker.Services;
 using issuetracker.ViewModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Slugify;
 
@@ -11,9 +12,12 @@ namespace issuetracker.mvc.Controllers;
 public class ProjectsController : Controller
 {
 	private readonly IProjectsService projectsService;
-	public ProjectsController(IProjectsService projectsService)
+	private readonly UserManager<AppUser> userManager;
+	public ProjectsController(IProjectsService projectsService, UserManager<AppUser> userManager)
 	{
 		this.projectsService = projectsService;
+
+		this.userManager = userManager;
 	}
 
 	[HttpGet]
@@ -28,7 +32,73 @@ public class ProjectsController : Controller
 	public async Task<IActionResult> Project(string id)
 	{
 		Project project = await projectsService.GetOneProjectAsync(slug: id);
-		return View(project);
+
+		List<Issue> unAssignedIssues = new();
+		List<Issue> overDueIssues = new();
+		List<Issue> openIssues = new();
+		List<Issue> closedIssues = new();
+
+		foreach (var issue in project.Issues)
+		{
+			if (issue.AssignedTo.Count() == 0)
+			{
+				unAssignedIssues.Add(issue);
+			}
+
+			if (issue.TargetResolutionDate > DateTime.UtcNow)
+			{
+				overDueIssues.Add(issue);
+			}
+
+			if (issue.Status == Status.Open)
+			{
+				openIssues.Add(issue);
+			}
+
+			if (issue.Status == Status.Closed)
+			{
+				closedIssues.Add(issue);
+			}
+
+		}
+
+
+		List<AssignedToUserViewModel> assignedToUserViewModels = new();
+
+		foreach (var user in project.AssignedTo)
+		{
+			var roles = await userManager.GetRolesAsync(user);
+
+			AssignedToUserViewModel assignedToUserViewModel = new()
+			{
+				Email = user.Email,
+				Name = $"{user.FirstName} {user.LastName}",
+				Id = user.Id,
+				Image = user.Image,
+				Roles = roles.ToList()
+			};
+
+			assignedToUserViewModels.Add(assignedToUserViewModel);
+		}
+
+		ProjectDetailViewModel projectDetailViewModel = new()
+		{
+			Id = project.Id.ToString(),
+			Name = project.Name,
+			StartDate = project.StartDate,
+			TargetEndDate = project.TargetEndDate,
+			ActualEndDate = project.ActualEndDate,
+			CreatedOn = project.CreatedOn,
+			CreatedBy = project.CreatedBy,
+			UnAssignedIssued = unAssignedIssues,
+			OverDueIssues = overDueIssues,
+			OpenIssues = openIssues,
+			ClosedIssue = closedIssues,
+			AssignedTo = assignedToUserViewModels
+		};
+
+
+		return View(projectDetailViewModel);
 	}
 
 
