d4282086b4c3216843eff37a675b7d7e2efa0f8c
mosaab-rady <mosp.rady@gmail.com>
2022-09-27 23:04:26 +0200

    add assignedtoUserViewModel

---
+++ issuetracker.ViewModels/UserViewModels/AssignedToUserViewModel.cs
@@ -0,0 +1,10 @@
+namespace issuetracker.ViewModels;
+
+public class AssignedToUserViewModel
+{
+	public string Id { get; set; }
+	public string Name { get; set; }
+	public string Image { get; set; }
+	public string Email { get; set; }
+	public List<string> Roles { get; set; }
+}
