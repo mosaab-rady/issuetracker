399706a19a9db0e0a40f0ad69d7c4d30b0622e35
mosaab-rady <mosp.rady@gmail.com>
2022-09-28 22:21:26 +0200

    add tags service

---
+++ issuetracker.Services/Services/ITagsServices.cs
@@ -0,0 +1,12 @@
+using issuetracker.Entities;
+
+namespace issuetracker.Services;
+
+public interface ITagsServices
+{
+	Task<IEnumerable<Tag>> GetAllTagsAsync();
+	Task<Tag> GetTagByIdAsync(Guid id);
+	Task CreateTagAsync(Tag tag);
+	Task UpdateTagByIdAsync(Guid id, Tag tag);
+	Task DeleteTagByIdAsync(Guid id);
+}
---
+++ issuetracker.Services/Services/TagsService.cs
@@ -0,0 +1,47 @@
+using issuetracker.Database;
+using issuetracker.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace issuetracker.Services;
+
+public class TagsService : ITagsServices
+{
+	private readonly PostgresContext context;
+
+	public TagsService(PostgresContext context)
+	{
+		this.context = context;
+	}
+
+	public async Task CreateTagAsync(Tag tag)
+	{
+		await context.Tags.AddAsync(tag);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task DeleteTagByIdAsync(Guid id)
+	{
+		var tag = await context.Tags.FindAsync(id);
+		context.Tags.Remove(tag);
+		await context.SaveChangesAsync();
+	}
+
+	public async Task<IEnumerable<Tag>> GetAllTagsAsync()
+	{
+		var tags = await context.Tags.ToListAsync();
+		return tags;
+	}
+
+	public async Task<Tag> GetTagByIdAsync(Guid id)
+	{
+		var tag = await context.Tags.Include(x => x.Issues).SingleOrDefaultAsync(x => x.Id == id);
+		return tag;
+	}
+
+	public async Task UpdateTagByIdAsync(Guid id, Tag tag)
+	{
+		var ETag = await context.Tags.FindAsync(id);
+		ETag = tag;
+		await context.SaveChangesAsync();
+	}
+}
