f27266a64ad2e70d0afd3e92c4fd37fe6c81cb36
mosaab-rady <mosp.rady@gmail.com>
2022-09-28 22:25:12 +0200

    add assign tag view model

---
+++ issuetracker.ViewModels/TagViewModels/AssignTagViewModel.cs
@@ -0,0 +1,9 @@
+namespace issuetracker.ViewModels;
+
+public class AssignTagViewModel
+{
+	public string TagId { get; set; }
+	public string Name { get; set; }
+	public string Color { get; set; }
+	public bool IsSelected { get; set; }
+}
