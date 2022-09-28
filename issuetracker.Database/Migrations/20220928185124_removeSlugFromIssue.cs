f0404a7f12448477b56e6266ef51c738baadc358
mosaab-rady <mosp.rady@gmail.com>
2022-09-28 22:14:15 +0200

    remove slug column from issue table

---
+++ issuetracker.Database/Migrations/20220928185124_removeSlugFromIssue.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace issuetracker.Database.Migrations
+{
+    public partial class removeSlugFromIssue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "slug",
+                table: "issues");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "slug",
+                table: "issues",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
