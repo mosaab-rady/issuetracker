9387fe17397e470c99bb65f5a74ef57e2bd7cc2b
mosaab-rady <mosp.rady@gmail.com>
2022-09-30 23:03:44 +0200

    add create tag property

--- issuetracker.ViewModels/IssueViewModels/CreateIssueViewModel.cs
+++ issuetracker.ViewModels/IssueViewModels/CreateIssueViewModel.cs
@@ -5,12 +5,21 @@ namespace issuetracker.ViewModels;
 
 public class CreateIssueViewModel
 {
+	public CreateIssueViewModel()
+	{
+		Tags = new List<AssignTagViewModel>();
+		projects = new List<AssignProjectViewModel>();
+	}
+
+
 	[Required]
 	public string Title { get; set; }
 
 	[Required]
 	public string Description { get; set; }
 	public List<AssignTagViewModel> Tags { get; set; }
+
+	public CreateTagViewModel CreateTagViewModel { get; set; }
 
 	[Required]
 	public string ProjectId { get; set; }
