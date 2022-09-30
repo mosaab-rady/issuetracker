728adb295bda16fc6f7cf4b84c2a5b6768ed03b2
mosaab-rady <mosp.rady@gmail.com>
2022-09-30 23:05:20 +0200

    add issue view model

---
+++ issuetracker.ViewModels/IssueViewModels/IssueViewModel.cs
@@ -0,0 +1,10 @@
+namespace issuetracker.ViewModels;
+
+public class IssueViewModel
+{
+	public string IssueId { get; set; }
+	public string Title { get; set; }
+	public string ProjectName { get; set; }
+	public string Status { get; set; }
+	public String Priority { get; set; }
+}
