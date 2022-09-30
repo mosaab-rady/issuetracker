490cbc4072d0169a456fead04f7d6583b9d8dcfd
mosaab-rady <mosp.rady@gmail.com>
2022-09-30 23:04:50 +0200

    add issue detail view model

---
+++ issuetracker.ViewModels/IssueViewModels/IssueDetailViewModel.cs
@@ -0,0 +1,26 @@
+using issuetracker.Entities;
+
+namespace issuetracker.ViewModels;
+
+public class IssueDetailViewModel
+{
+	public IssueDetailViewModel()
+	{
+		AssignedTo = new List<AssignedToUserViewModel>();
+		Tags = new List<Tag>();
+		Priority = new Priority();
+	}
+	public string IssueId { get; set; }
+	public string Title { get; set; }
+	public string Description { get; set; }
+	public DateTime CreatedOn { get; set; }
+	public DateTime TargetResolutionDate { get; set; }
+	public string CreatedBy { get; set; }
+	public DateTime ActualResolutionDate { get; set; }
+	public string ProjectName { get; set; }
+	public string Status { get; set; }
+	public Priority Priority { get; set; }
+	public string ResolutionSummary { get; set; }
+	public List<AssignedToUserViewModel> AssignedTo { get; set; }
+	public List<Tag> Tags { get; set; }
+}
