a0c4364ea108e09609241a1d0d6f06d868d04120
mosaab-rady <mosp.rady@gmail.com>
2022-10-01 23:05:22 +0200

    change class name of assignuser

--- issuetracker.ViewModels/RolesViewModels/EditRoleViewModel.cs
+++ issuetracker.ViewModels/RolesViewModels/EditRoleViewModel.cs
@@ -4,9 +4,9 @@ public class EditRoleViewModel
 {
 	public EditRoleViewModel()
 	{
-		Users = new List<AssignedToUserViewModel>();
+		Users = new List<AssignUserViewModel>();
 	}
 	public string Id { get; set; }
 	public string Name { get; set; }
-	public List<AssignedToUserViewModel> Users { get; set; }
+	public List<AssignUserViewModel> Users { get; set; }
 }
