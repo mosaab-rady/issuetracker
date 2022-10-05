c4e10650cd250fe53069cf82d66c90696c9aa01b
mosaab-rady <mosp.rady@gmail.com>
2022-10-05 23:28:34 +0200

    rename file

--- issuetracker.ViewModels/UserViewModels/EditProjectAssignedToUser.cs
+++
@@ -1,8 +0,0 @@
-namespace issuetracker.ViewModels;
-
-public class EditProjectAssignedToUserViewModel
-{
-	public string ProjectId { get; set; }
-	public string ProjectName { get; set; }
-	public bool IsSelected { get; set; }
-}
