aa96d8dd542dde2286a66965af79fb9bef280852
mosaab-rady <mosp.rady@gmail.com>
2022-10-05 23:27:33 +0200

    add edit assigned roles to the user view model

---
+++ issuetracker.ViewModels/UserViewModels/EditRoleInUserViewModel.cs
@@ -0,0 +1,8 @@
+namespace issuetracker.ViewModels;
+
+public class EditRoleInUserViewModel
+{
+	public string RoleId { get; set; }
+	public string Name { get; set; }
+	public bool IsSelected { get; set; }
+}
