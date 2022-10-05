f0a3b4a68c3f0984ee050301e0b07be10f6825f7
mosaab-rady <mosp.rady@gmail.com>
2022-10-05 23:26:26 +0200

    add edit assigned projects to the user view model

---
+++ issuetracker.ViewModels/UserViewModels/EditProjectAssignedToUser.cs
@@ -0,0 +1,8 @@
+namespace issuetracker.ViewModels;
+
+public class EditProjectAssignedToUserViewModel
+{
+	public string ProjectId { get; set; }
+	public string ProjectName { get; set; }
+	public bool IsSelected { get; set; }
+}
