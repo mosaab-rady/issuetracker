7af56811e0807c2f00cb8d9c38687e9f57335209
mosaab-rady <mosp.rady@gmail.com>
2022-10-06 23:29:45 +0200

    add dashboard view

--- issuetracker.mvc/Controllers/HomeController.cs
+++ issuetracker.mvc/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using issuetracker.mvc.Models;
 using issuetracker.Services;
 using Microsoft.AspNetCore.Authorization;
+using issuetracker.ViewModels;
+using issuetracker.Entities;
 
 namespace issuetracker.mvc.Controllers;
 
@@ -11,20 +13,54 @@ public class HomeController : Controller
 {
 	private readonly ILogger<HomeController> _logger;
 
-	public HomeController(ILogger<HomeController> logger)
+	private readonly IIssuesService issuesService;
+
+	public HomeController(ILogger<HomeController> logger, IProjectsService projectsService, IIssuesService issuesService)
 	{
 		_logger = logger;
+		this.issuesService = issuesService;
 	}
 
-	public IActionResult Index()
+	public async Task<IActionResult> Index()
 	{
-		return View();
+		DashboardViewModel model = new();
+
+		foreach (var issue in await issuesService.GetAllIssuesAsync())
+		{
+			IssueViewModel issueViewModel = new()
+			{
+				IssueId = issue.Id.ToString(),
+				Priority = issue.Priority ?? new Priority() { Color = "#6699ff", Name = "Not Set" },
+				Status = Enum.GetName(typeof(Status), issue.Status),
+				ProjectName = issue.Project.Name,
+				Title = issue.Title
+			};
+
+			if (issue.AssignedTo.Count() == 0)
+			{
+				model.UnAssignedIssues.Add(issueViewModel);
+			}
+
+			if (issue.TargetResolutionDate < DateTime.UtcNow && issue.Status == Status.Open)
+			{
+				model.OverDueIssues.Add(issueViewModel);
+			}
+
+			if (DateTime.UtcNow < issue.CreatedOn.AddDays(5))
+			{
+				model.RecentlyOpenedIssues.Add(issueViewModel);
+			}
+
+			if (issue.Status == Status.Closed)
+			{
+				model.ClosedIssues.Add(issueViewModel);
+			}
+
+		}
+
+		return View(model);
 	}
 
-	public IActionResult Privacy()
-	{
-		return View();
-	}
 
 	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 	public IActionResult Error()
