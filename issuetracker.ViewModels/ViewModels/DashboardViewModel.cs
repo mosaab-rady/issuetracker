7cc17f8a63a31d43e02f852efb8e389b45d12133
mosaab-rady <mosp.rady@gmail.com>
2022-10-07 21:47:02 +0200

    add project overview view model

--- issuetracker.ViewModels/ViewModels/DashboardViewModel.cs
+++ issuetracker.ViewModels/ViewModels/DashboardViewModel.cs
@@ -7,10 +7,10 @@ public class DashboardViewModel
 		OverDueIssues = new List<IssueViewModel>();
 		RecentlyOpenedIssues = new List<IssueViewModel>();
 		UnAssignedIssues = new List<IssueViewModel>();
-		ClosedIssues = new List<IssueViewModel>();
+		Projects = new List<ProjectOverviewViewModel>();
 	}
+	public List<ProjectOverviewViewModel> Projects { get; set; }
 	public List<IssueViewModel> OverDueIssues { get; set; }
 	public List<IssueViewModel> RecentlyOpenedIssues { get; set; }
 	public List<IssueViewModel> UnAssignedIssues { get; set; }
-	public List<IssueViewModel> ClosedIssues { get; set; }
 }
