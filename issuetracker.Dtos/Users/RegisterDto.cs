9b834d70cc2911e6e7a83e2919856b54aaa9e37d
mosaab-rady <mosp.rady@gmail.com>
2022-11-06 19:47:26 +0200

    add register dto

---
+++ issuetracker.Dtos/Users/RegisterDto.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+
+namespace issuetracker.Dtos;
+
+public class RegisterDto
+{
+	[Required]
+	public string FirstName { get; set; }
+
+	[Required]
+	public string LastName { get; set; }
+
+	[Required]
+	public string Email { get; set; }
+
+	[Required]
+	public string Password { get; set; }
+
+	[Required]
+	[Compare("Password", ErrorMessage = "Password and confirm password must be the same.")]
+	public string ConfirmPassword { get; set; }
+
+	public IFormFile Image { get; set; }
+}
