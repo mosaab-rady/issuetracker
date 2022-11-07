9f509c7c5f317faf2a3f488c9255070370303fd1
mosaab-rady <mosp.rady@gmail.com>
2022-11-07 21:45:19 +0200

    remove assigned users from projectDto

--- issuetracker.Dtos/Projects/ProjectDto.cs
+++ issuetracker.Dtos/Projects/ProjectDto.cs
@@ -1,10 +1,7 @@
 ﻿namespace issuetracker.Dtos;
 public class ProjectDto
 {
-	public ProjectDto()
-	{
-		AssignedTo = new List<UserDto>();
-	}
+
 	public string Id { get; set; }
 	public string Name { get; set; }
 	public DateTime StartDate { get; set; }
@@ -12,5 +9,4 @@ public class ProjectDto
 	public DateTime ActualEndDate { get; set; }
 	public DateTime CreatedOn { get; set; }
 	public string CreatedBy { get; set; }
-	public List<UserDto> AssignedTo { get; set; }
 }
