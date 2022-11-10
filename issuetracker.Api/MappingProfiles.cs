7ed9977601600c37e07c34c86aee457d52a774a6
mosaab-rady <mosp.rady@gmail.com>
2022-11-10 22:53:43 +0200

    add mapping profiles

--- issuetracker.Api/MappingProfiles.cs
+++ issuetracker.Api/MappingProfiles.cs
@@ -12,6 +12,18 @@ public class MappingProfiles : Profile
 		CreateMap<AppUser, UserDto>();
 		CreateMap<UserDto, AppUser>();
 		CreateMap<CreateProjectDto, Project>();
-		CreateMap<Project,ProjectWithUsersDto>();
+		CreateMap<Project, ProjectWithUsersDto>();
+		CreateMap<Priority, PriorityDto>();
+		CreateMap<CreatePriorityDto, Priority>();
+		CreateMap<Tag, TagDto>();
+		CreateMap<CreateTagDto, Tag>();
+
+
+		CreateMap<Issue, IssueDto>()
+		.ForMember(issueDto => issueDto.Status, opt => opt.MapFrom(src => Enum.GetName(typeof(Status), src.Status)))
+		.ForMember(issueDto => issueDto.ProjectName, opt => opt.MapFrom(src => src.Project.Name));
+
+		CreateMap<CreateIssueDto, Issue>();
+
 	}
 }
