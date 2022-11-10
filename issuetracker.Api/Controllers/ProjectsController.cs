d7ccfe0a48d9f5858d567919ab984902a7e6d8f1
mosaab-rady <mosp.rady@gmail.com>
2022-11-10 22:52:34 +0200

    add edit and get users action

--- issuetracker.Api/Controllers/ProjectsController.cs
+++ issuetracker.Api/Controllers/ProjectsController.cs
@@ -43,9 +43,10 @@ public class ProjectsController : ControllerBase
 
 
 	[HttpGet("{id}")]
-	public async Task<IActionResult> GetProjectById(string id)
+	[Authorize(Roles = "manager")]
+	public async Task<IActionResult> GetProjectById(Guid id)
 	{
-		var project = await projectsService.GetProjectByIdWithUsersAsync(Guid.Parse(id));
+		var project = await projectsService.GetProjectByIdAsync(id);
 
 		if (project is null)
 		{
@@ -55,16 +56,17 @@ public class ProjectsController : ControllerBase
 		}
 
 
-		ProjectWithUsersDto projectWithUsersDto = mapper.Map<ProjectWithUsersDto>(project);
+		// ProjectWithUsersDto projectWithUsersDto = mapper.Map<ProjectWithUsersDto>(project);
+		ProjectDto projectDto = mapper.Map<ProjectDto>(project);
 
-		foreach (var user in projectWithUsersDto.AssignedTo)
-		{
-			AppUser appUser = mapper.Map<AppUser>(user);
-			var roles = (await userManager.GetRolesAsync(appUser)).ToList();
-			user.Roles = roles;
-		}
+		// foreach (var user in projectWithUsersDto.AssignedTo)
+		// {
+		// 	AppUser appUser = mapper.Map<AppUser>(user);
+		// 	var roles = (await userManager.GetRolesAsync(appUser)).ToList();
+		// 	user.Roles = roles;
+		// }
 
-		return Ok(projectWithUsersDto);
+		return Ok(projectDto);
 	}
 
 	[Authorize(Roles = "manager")]
@@ -89,17 +91,16 @@ public class ProjectsController : ControllerBase
 		}
 
 		return CreatedAtAction(
-			nameof(GetProjectById),
-			new { id = project.Id },
+			nameof(CreateProject),
 			mapper.Map<ProjectDto>(project));
 	}
 
 
 	[Authorize(Roles = "manager")]
 	[HttpDelete("{id}")]
-	public async Task<IActionResult> DeleteProjectById(string id)
+	public async Task<IActionResult> DeleteProjectById(Guid id)
 	{
-		Project project = await projectsService.GetProjectByIdAsync(Guid.Parse(id));
+		Project project = await projectsService.GetProjectByIdAsync(id);
 
 		if (project is null)
 		{
@@ -113,11 +114,13 @@ public class ProjectsController : ControllerBase
 		return NoContent();
 	}
 
+
+
 	[Authorize(Roles = "manager")]
 	[HttpPut("{id}")]
-	public async Task<IActionResult> UpdateProjectById(UpdateProjectDto model, string id)
+	public async Task<IActionResult> UpdateProjectById(UpdateProjectDto model, Guid id)
 	{
-		var existingProject = await projectsService.GetProjectByIdAsync(Guid.Parse(id));
+		var existingProject = await projectsService.GetProjectByIdAsync(id);
 
 		if (existingProject is null)
 		{
@@ -147,6 +150,76 @@ public class ProjectsController : ControllerBase
 	}
 
 
+	// get users assigned to project
+	[HttpGet("{projectId}/users")]
+	public async Task<IActionResult> GetUsersInProject(Guid projectId)
+	{
+		Project project = await projectsService.GetProjectByIdAsync(projectId);
+
+		if (project is null)
+		{
+			return Problem(
+				detail: $"No Project found with this Id '{projectId}'.",
+				statusCode: StatusCodes.Status404NotFound);
+		}
+
+		List<AppUser> usersInProject = await projectsService.GetUsersInProjectAsync(projectId);
+
+		List<UserDto> userDtos = mapper.Map<List<UserDto>>(usersInProject);
+
+		foreach (var user in userDtos)
+		{
+			user.Roles = (await userManager.GetRolesAsync(mapper.Map<AppUser>(user))).ToList();
+		}
+
+		return Ok(userDtos);
+
+	}
+
+
+	// Edit Users In Project
+	[HttpPost("{projectId}/EditUsers")]
+	[Authorize(Roles = "manager")]
+	public async Task<IActionResult> EditUsersInProject(List<AssignUserDto> assignUserDtos, Guid projectId)
+	{
+		Project project = await projectsService.GetProjectByIdAsync(projectId);
+
+		if (project is null)
+		{
+			return Problem(
+				detail: $"No Project found with this Id '{projectId}'.",
+				statusCode: StatusCodes.Status404NotFound);
+		}
+
+		List<AppUser> UsersInProject = await projectsService.GetUsersInProjectAsync(projectId);
+
+		foreach (AssignUserDto assignUserDto in assignUserDtos)
+		{
+			AppUser appUser = await userManager.FindByIdAsync(assignUserDto.UserId);
+
+			if (appUser is null)
+			{
+				return Problem(
+					detail: $"No user found with this Id '{assignUserDto.UserId}'.",
+					statusCode: StatusCodes.Status404NotFound);
+			}
+
+			if (assignUserDto.IsSelected && !UsersInProject.Contains(appUser))
+			{
+				await projectsService.AssignUser(appUser, projectId);
+			}
+			else if (!assignUserDto.IsSelected && UsersInProject.Contains(appUser))
+			{
+				await projectsService.UnAssignUser(appUser, projectId);
+			}
+		}
+
+		UsersInProject = await projectsService.GetUsersInProjectAsync(projectId);
+
+		List<UserDto> userDtos = mapper.Map<List<UserDto>>(UsersInProject);
+
+		return Ok(userDtos);
+	}
 
 	private string Slugify(string name)
 	{
